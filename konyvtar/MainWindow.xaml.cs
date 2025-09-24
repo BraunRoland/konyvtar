ad4ca1faf37eca1c3d38ee8e58ab01105d405f5b
Petrik Diak <diak@petrik.hu>
2025-09-24 12:25:55 +0200

    Add project files.

---
+++ konyvtar/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace konyvtar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		StreamReader sr = new StreamReader("olvasok.txt");
+		StreamWriter sw = new StreamWriter("olvasok.txt", false);
+		List<Olvaso> olvasok = new List<Olvaso>();
+		public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void  fajlbeolvasas()
+        {
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                string[] adatok = sor.Split(';');
+                Olvaso o = new Olvaso(adatok[0], int.Parse(adatok[1]), adatok[2], bool.Parse(adatok[3]), bool.Parse(adatok[4]), adatok[5]);
+                olvasok.Add(o);
+			}
+		}
+
+        private void fajliras()
+        {
+
+        }
+
+
+    }
+}
---
+++ konyvtar/Olvaso.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvtar
+{
+    class Olvaso
+    {
+        private string nev;
+        private int eletkor;
+        private string mufaj;
+        private bool hirlevel;
+        private bool sms;
+        private string tagsag;
+
+		public Olvaso(string nev, int eletkor, string mufaj, bool hirlevel, bool sms, string tagsag)
+		{
+			this.Nev = nev;
+			this.Eletkor = eletkor;
+			this.Mufaj = mufaj;
+			this.Hirlevel = hirlevel;
+			this.Sms = sms;
+			this.Tagsag = tagsag;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Hirlevel { get => hirlevel; set => hirlevel = value; }
+		public bool Sms { get => sms; set => sms = value; }
+		public string Tagsag { get => tagsag; set => tagsag = value; }
+	}
+}
